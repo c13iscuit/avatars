7589b3be6b6af899e23e02566eeace0cf3f40cba
Aly Barnett <barnett.chri@gmail.com>
2014-08-22 14:48:39 -0400

    added user model/migration -rmvd redundant test folder

---
+++ db/migrate/20140822180633_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :username, null: false
+      t.string :avatar, null: false
+      t.timestamps
+    end
+  end
+end
