558533edad9a878b504180260358c8ed843b5e77
Aly Barnett <barnett.chri@gmail.com>
2014-08-26 14:19:27 -0400

    Added to users: pw_hash, pw_salt for auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140822184917) do
+ActiveRecord::Schema.define(version: 20140826181628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,10 +24,12 @@ ActiveRecord::Schema.define(version: 20140822184917) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "username",   null: false
-    t.string   "avatar",     null: false
+    t.string   "username",      null: false
+    t.string   "avatar",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_hash"
+    t.string   "password_salt"
   end
 
 end
