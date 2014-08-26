deb2bfbdd8396740d2657977814f6c0c7f6098fd
Aly Barnett <barnett.chri@gmail.com>
2014-08-26 16:28:25 -0400

    Finished session handling

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root "users#index"
 
   get "log_in" => "sessions#new", as: "log_in"
+  get "log_out" => "sessions#destroy", as: "log_out"
   get "sign_up" => "users#new", as: "sign_up"
 
   resources :users, only: [:index, :new, :create, :edit, :update]
